05ac0727c029e452343a148c43dbd9a2fdcbc3c1
Ryan Stracener <ryanstrace@outlook.com>
2025-11-17 23:18:26 -0600

    refactored EmulationServer tests to use test fixtures

--- tests/test_emulation_server.py
+++ tests/test_emulation_server.py
@@ -3,213 +3,165 @@ import pytest
 import rtmc_client as rtmc
 import socket
 
-# Test the behavior of a proper authentication
-def test_auth_success():
+@pytest.fixture
+def emulator():
     api_token = "dummy_token"
 
+    # Start the emulator
     emulator = rtmc.EmulationServer(api_token)
     emulator.start()
 
+    try:
+        yield emulator
+    finally:
+        emulator.stop()
+
+
+
+@pytest.fixture
+def tcp_socket(emulator):
     # Create TCP client
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((emulator.ipv4_addr, emulator.tcp_port))
 
-    # Send data and assert what the response should be
-    sock.sendall(f"auth {api_token}".encode())
-    response = json.loads(sock.recv(1024).decode())
-    assert response.get("status") == "OKAY"
+    try:
+        yield sock
+    finally:
+        sock.close()    
 
-    emulator.stop()
+
+
+@pytest.fixture
+def udp_socket(emulator):
+    # Create UDP client
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(0.1)
+
+    try:
+        yield sock
+    finally:
+        sock.close()
+
+
+
+# Test the behavior of a proper authentication
+def test_auth_success(emulator, tcp_socket):
+    tcp_socket.sendall(f"auth {emulator.api_token}".encode())
+    response = json.loads(tcp_socket.recv(1024).decode())
+
+    assert response.get("status") == "OKAY"
 
 
 
 # Test the behavior of an incorrect token
-def test_auth_wrong_token():
-    emulator = rtmc.EmulationServer("correct_token")
-    emulator.start()
+def test_auth_wrong_token(emulator, tcp_socket):
+    tcp_socket.sendall("auth wrong_token".encode())
+    response = json.loads(tcp_socket.recv(1024).decode())
 
-    # Create TCP client
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((emulator.ipv4_addr, emulator.tcp_port))
-
-    # Send data and assert what the response should be
-    sock.sendall("auth wrong_token".encode())
-    response = json.loads(sock.recv(1024).decode())
     assert response.get("status") == "ERROR"
-
-    emulator.stop()
 
 
 
 # Test the behavior of sending something other than `auth <token>`
-def test_auth_malformed():
-    emulator = rtmc.EmulationServer("correct_token")
-    emulator.start()
+def test_auth_malformed(emulator, tcp_socket):
+    tcp_socket.sendall("xyz".encode()) # Send "xyz" instead of "auth <token>"
+    response = json.loads(tcp_socket.recv(1024).decode())
 
-    # Create TCP client
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((emulator.ipv4_addr, emulator.tcp_port))
-
-    # Send data and assert what the response should be
-    sock.sendall("xyz".encode()) # Send "xyz" instead of "auth <token>"
-    response = json.loads(sock.recv(1024).decode())
     assert response.get("status") == "ERROR"
-
-    emulator.stop()
 
 
 
 # Test to ensure that the connection is closed if auth is failed
-def test_auth_closes_connection():
-    emulator = rtmc.EmulationServer("correct_token")
-    emulator.start()
-
-    # Create TCP client
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((emulator.ipv4_addr, emulator.tcp_port))
-
+def test_auth_closes_connection(emulator, tcp_socket):
     # Send wrong token
-    sock.sendall("auth wrong_token".encode())
-    response = json.loads(sock.recv(1024).decode())
+    tcp_socket.sendall("auth wrong_token".encode())
+    response = json.loads(tcp_socket.recv(1024).decode())
     assert response.get("status") == "ERROR"
 
-    # Make sure the socket is closed
+    # Ensure the socket is closed
     socket_closed = False
     try:
         # Try sending the correct token
-        sock.sendall("auth correct_token".encode())
-        response = json.loads(sock.recv(1024).decode())
+        tcp_socket.sendall("auth correct_token".encode())
+        response = json.loads(tcp_socket.recv(1024).decode())
     except Exception:
         socket_closed = True
     
     assert socket_closed
 
-    emulator.stop()
-
 
 
 # Test running the auth command after authenticating
-def test_auth_twice():
-    api_token = "dummy_token"
-
-    emulator = rtmc.EmulationServer(api_token)
-    emulator.start()
-
-    # Create TCP client
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((emulator.ipv4_addr, emulator.tcp_port))
-
-    # Send the auth command twice
+def test_auth_twice(emulator, tcp_socket):
     for _ in range(2):
         # Send data and assert what the response should be
-        sock.sendall(f"auth {api_token}".encode())
-        response = json.loads(sock.recv(1024).decode())
+        tcp_socket.sendall(f"auth {emulator.api_token}".encode())
+        response = json.loads(tcp_socket.recv(1024).decode())
         assert response.get("status") == "OKAY"
-
-    emulator.stop()
 
 
 
 # Test the discover command over the TCP socket
-def test_discover_tcp():
-    api_token = "dummy_token"
-
-    emulator = rtmc.EmulationServer(api_token)
-    emulator.start()
-
-    # Create TCP client
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((emulator.ipv4_addr, emulator.tcp_port))
-
+def test_discover_tcp(emulator, tcp_socket):
     # Authenticate
-    sock.sendall(f"auth {api_token}".encode())
-    response = json.loads(sock.recv(1024).decode())
+    tcp_socket.sendall(f"auth {emulator.api_token}".encode())
+    response = json.loads(tcp_socket.recv(1024).decode())
     assert response.get("status") == "OKAY"
 
     # Send the discover query
-    sock.sendall("discover rtmc*".encode())
-    response = json.loads(sock.recv(1024).decode())
+    tcp_socket.sendall("discover rtmc*".encode())
+    response = json.loads(tcp_socket.recv(1024).decode())
 
     # Verify the response data
-    assert response.get("service") == "rtmc-tcp-1.0-emulator"
+    assert response.get("service") == emulator.service
     assert response.get("port") == emulator.tcp_port
     assert response.get("device") == emulator.device
     assert response.get("serial_number") == emulator.serial_number
     assert response.get("firmware_version") == emulator.firmware_version
 
-    emulator.stop()
-
 
 
 # Test the discover command over the TCP socket
-def test_discover_tcp_bad_query():
-    api_token = "dummy_token"
-
-    emulator = rtmc.EmulationServer(api_token)
-    emulator.start()
-
-    # Create TCP client
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((emulator.ipv4_addr, emulator.tcp_port))
-
+def test_discover_tcp_bad_query(emulator, tcp_socket):
     # Authenticate
-    sock.sendall(f"auth {api_token}".encode())
-    response = json.loads(sock.recv(1024).decode())
+    tcp_socket.sendall(f"auth {emulator.api_token}".encode())
+    response = json.loads(tcp_socket.recv(1024).decode())
     assert response.get("status") == "OKAY"
 
     # Send the discover query
-    sock.sendall("discover bad_query".encode())
-    response = sock.recv(1024).decode()
+    tcp_socket.sendall("discover bad_query".encode())
+    response = tcp_socket.recv(1024).decode()
 
     # Verify the response data
     assert response == "{}"
 
-    emulator.stop()
-
 
 
 # Test the discover command via UDP multicast
-def test_discover_udp():
-    emulator = rtmc.EmulationServer("dummy_token")
-    emulator.start()
+def test_discover_udp(emulator, udp_socket):
+    udp_socket.sendto(b"discover rtmc*", (emulator.udp_multicast_group, emulator.udp_port))
 
-    # Create UDP client
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(0.1)
-    sock.sendto(b"discover rtmc*", (emulator.udp_multicast_group, emulator.udp_port))
-
-    # Parse response
-    response, server = sock.recvfrom(1024)
+    response, server = udp_socket.recvfrom(1024)
     json_data = json.loads(response.decode())
 
     # Verify the response data
-    assert json_data.get("service") == "rtmc-tcp-1.0-emulator"
+    assert json_data.get("service") == emulator.service
     assert json_data.get("port") == emulator.tcp_port
     assert json_data.get("device") == emulator.device
     assert json_data.get("serial_number") == emulator.serial_number
     assert json_data.get("firmware_version") == emulator.firmware_version
 
-    emulator.stop()
-
 
 
 # Test the discover command via UDP multicast
-def test_discover_udp_bad_query():
-    emulator = rtmc.EmulationServer("dummy_token")
-    emulator.start()
+def test_discover_udp_bad_query(emulator, udp_socket):
+    udp_socket.sendto(b"bad query", (emulator.udp_multicast_group, emulator.udp_port))
 
-    # Create UDP client
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(0.1)
-    sock.sendto(b"bad query", (emulator.udp_multicast_group, emulator.udp_port))
-
-    # Parse response
+    # Make sure there's no response (check for timeout)
     socket_timeout = False
     try:
-        response, server = sock.recvfrom(1024)
+        response, server = udp_socket.recvfrom(1024)
     except socket.timeout:
         socket_timeout = True
     
     assert socket_timeout
-
-    emulator.stop()
